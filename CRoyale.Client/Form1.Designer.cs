b27dbeeee7ebf5a7bf4b97345cda11fdbcaf1010
alejandromtz04 <not4lmas@gmail.com>
2024-08-21 13:52:33 -0600

    Adding code to the game

--- CRoyale.Client/Form1.Designer.cs
+++ CRoyale.Client/Form1.Designer.cs
@@ -29,9 +29,11 @@
 		private void InitializeComponent()
 		{
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.AttackButtom = new System.Windows.Forms.Button();
             this.labelHP = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.MiHP = new System.Windows.Forms.ProgressBar();
+            this.MiVida = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +41,7 @@
             // 
             this.pictureBox1.ImageLocation = "C:\\Users\\Not4l\\Documents\\programacion\\juegoProgra2\\Royale-master\\CRoyale.Client\\I" +
     "mages\\pekka vs golem.jpg";
-            this.pictureBox1.Location = new System.Drawing.Point(-9, -7);
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(510, 495);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -47,20 +49,20 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // button2
+            // AttackButtom
             // 
-            this.button2.Location = new System.Drawing.Point(92, 382);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Atacar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.AttackButtom.Location = new System.Drawing.Point(200, 202);
+            this.AttackButtom.Name = "AttackButtom";
+            this.AttackButtom.Size = new System.Drawing.Size(75, 23);
+            this.AttackButtom.TabIndex = 2;
+            this.AttackButtom.Text = "Atacar";
+            this.AttackButtom.UseVisualStyleBackColor = true;
+            this.AttackButtom.Click += new System.EventHandler(this.button2_Click);
             // 
             // labelHP
             // 
             this.labelHP.AutoSize = true;
-            this.labelHP.Location = new System.Drawing.Point(339, 71);
+            this.labelHP.Location = new System.Drawing.Point(368, 171);
             this.labelHP.Name = "labelHP";
             this.labelHP.Size = new System.Drawing.Size(56, 16);
             this.labelHP.TabIndex = 3;
@@ -69,11 +71,28 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(342, 111);
+            this.progressBar1.Location = new System.Drawing.Point(351, 202);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(100, 23);
             this.progressBar1.TabIndex = 4;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // MiHP
+            // 
+            this.MiHP.Location = new System.Drawing.Point(27, 202);
+            this.MiHP.Name = "MiHP";
+            this.MiHP.Size = new System.Drawing.Size(100, 23);
+            this.MiHP.TabIndex = 5;
+            // 
+            // MiVida
+            // 
+            this.MiVida.AutoSize = true;
+            this.MiVida.Location = new System.Drawing.Point(49, 171);
+            this.MiVida.Name = "MiVida";
+            this.MiVida.Size = new System.Drawing.Size(50, 16);
+            this.MiVida.TabIndex = 6;
+            this.MiVida.Text = "Mi vida";
+            this.MiVida.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Form1
             // 
@@ -82,9 +101,11 @@
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(494, 487);
+            this.Controls.Add(this.MiVida);
+            this.Controls.Add(this.MiHP);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.labelHP);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AttackButtom);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
@@ -98,9 +119,11 @@
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AttackButtom;
         private System.Windows.Forms.Label labelHP;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar MiHP;
+        private System.Windows.Forms.Label MiVida;
     }
 }
 
--- CRoyale.Client/Form1.cs
+++ CRoyale.Client/Form1.cs
@@ -18,10 +18,18 @@ namespace CRoyale.Client
 			InitializeComponent();
 		}
         Card card = new Card();
+        myCard myCard = new myCard();
+        Random random = new Random();
         private void Form1_Load(object sender, EventArgs e)
 		{
+            progressBar1.Maximum = card.HPBase;
+            progressBar1.Value = card.HP;
+            labelHP.Text = $"Vida restante: {card.HP}";
 
-		}
+            MiHP.Maximum = myCard.HPMyCard;
+            MiHP.Value = myCard.DamageToMe;
+            MiVida.Text = $"Mi vida: {myCard.HPMyCard}";
+        }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -30,28 +38,52 @@ namespace CRoyale.Client
 
         private void button2_Click(object sender, EventArgs e)
         {
-			int damageAmount = 10;
-
+			int damageAmount = random.Next(1, 11);
+            int damageToMe = random.Next(1, 11);
 			card.HP -= damageAmount;
 
 			if (card.HP <= 0)
 			{
 				card.HP = 0;
 			}
+            myCard.HPMyCard -= damageToMe;
+            if(myCard.HPMyCard <= 0)
+            {
+                myCard.HPMyCard = 0;
+            }
 
-			
+            progressBar1.Value = card.HP;
+            labelHP.Text = $"Vida restante: {card.HP}";
+
+            MiHP.Value = myCard.HPMyCard;
+            MiVida.Text = $"Mi vida: {myCard.HPMyCard}";
+
+
+            MessageBox.Show($"Realizaste un ataque per recibiste: {damageToMe} de daño");
+
+            // Finaliza el juego:
+            if(myCard.HPMyCard == 0 || card.HP == 0)
+            {
+                string winner = myCard.HPMyCard > 0 ? "Jugador" : "Enemigo";
+                MessageBox.Show($"{winner} ha ganado el juego!", "Juego Terminado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
         }
 
         private void progressBar1_Click(object sender, EventArgs e)
         {
-            progressBar1.Maximum = card.HP;
-            progressBar1.Value = card.HP;
-            labelHP.Text = $"Vida restante: {card.HP}";
+           
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
